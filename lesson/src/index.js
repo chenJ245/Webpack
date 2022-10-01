09a5ba6f3ec18b5aec17af34380fcfea6f5d8103
chenJ245 <2452930499@qq.com>
2022-10-01 13:31:13 +0800

    feat: 使用 Loader 打包静态资源

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,22 +1,14 @@
-// ES Moudule 模块引入方式
-// CommonJS 模块引入规范
-// CMD
-// AMD
+import avatar from './avat.jpg'
+// import Header from './header.vue'
+import style from './index.scss';
+import createAvatar from './createAvatar';
 
-// webpack 模块打包工具
-// js 模块打包工具 -> 任何形式的文件
+createAvatar();
 
+// var avatar = require('./avat.jpg')
+var img = new Image();
+img.src = avatar;
+img.classList.add('style.avatar');
 
-// import Header from './Header';
-// import Sidebar from './Sidebar';
-// import Content from './Content';
-// var style = require('./index.css');
-// import style from './index.css';
-
-var Header = require('./header.js');
-var Sidebar = require('./sidebar.js');
-var Content = require('./content.js');
-
-new Header();
-new Sidebar();
-new Content();
+var root = document.getElementById('root');
+root.append(img);
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -5,6 +5,35 @@ module.exports = {
   entry: {
     main: './src/index.js'
   },
+  module: {
+    rules: [{
+      test: /\.(jpg|png|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          // placeholder 占位符
+          name: '[name].[ext]',
+          outputPath: 'images/',
+          limit: 10240
+        }
+      }
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+            modules: true
+          }
+        },
+        'sass-loader',
+        'postcss-loader'
+      ]
+    }]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
